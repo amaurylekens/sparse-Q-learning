b0e88a2cb651c760f6475e221fdf4678d2dcbb93
Victor Goossens <victor1goossens@gmail.com>
2020-01-02 00:42:48 +0100

    Add proba for prey to stay on same case (proba=0.2)

--- prey.py
+++ prey.py
@@ -2,6 +2,9 @@ import random
 
 
 class Prey:
+
+    no_move_proba = 0.2
+
     def __init__(self, n_action):
 
         """
@@ -15,10 +18,12 @@ class Prey:
     def get_action_choice(self, free_cells):
 
         """
-        return the prey action choice (random)
+        return the prey action choice (stays on same position with proba 'no_move_proba', else random between free adjacent cells)
         :param free_cells: the neighbor cells
         """
-
-        a_t = free_cells[random.randint(0, len(free_cells)-1)]
+        if (random.uniform(0, 1) < Prey.no_move_proba):
+            a_t = (0,0)
+        else:
+            a_t = free_cells[random.randint(0, len(free_cells)-1)]
 
         return a_t
